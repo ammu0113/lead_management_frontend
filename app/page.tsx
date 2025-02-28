a4b0a4658ec7afb3e3049d6445e61c60f3de1300
Amulya Bandla <amulya.bandla01@gmail.com>
2025-02-28 16:08:56 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -9,13 +9,13 @@ export default function Home() {
       <div className="relative">
         {/* Hero section with green background */}
         <div className="relative bg-[#d8e0a8] py-16 px-4 sm:px-6 lg:px-8">
-          <div className="max-w-5xl mx-auto flex items-center justify-center space-x-6">
+          <div className="max-w-5xl mx-auto flex items-center justify-center md:justify-end sm:justify-end space-x-6">
             {/* Left Image */}
             {/* <div className="w-1/3"> */}
             <Image
               src={leftImage}
               alt="Decorative"
-              className="absolute left-0 h-full w-[100px] object-cover"
+              className="absolute left-0 h-full w-[100px] object-cover hidden md:block"
               style={{
                 width: "auto",
               }}
